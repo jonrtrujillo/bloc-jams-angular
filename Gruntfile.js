c4a1e8984058346b49c22fd4711ac83853a72c2f
Jonathan Trujillo <jtrujillo@gmail.com>
2017-03-09 13:48:51 -0800

    begin 2

---
+++ Gruntfile.js
@@ -0,0 +1,5 @@
+module.exports = function(grunt) {
+    grunt.registerTask('default', 'Testing out Grunt logging and task callbacks', function() {
+        grunt.log.writeln('This is our first Grunt task!');
+        });
+    };
