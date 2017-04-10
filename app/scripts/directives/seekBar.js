3945d2b3e78a1535cccbe275709ebdda67ed4c73
Jonathan Trujillo <jtrujillo@gmail.com>
2017-04-10 00:38:55 -0700

    check9asign

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -31,6 +31,12 @@
              scope.fillStyle = function() {
                  return {width: percentString()};
              };
+             scope.thumbStyle = function () {
+               return {
+                 left: percentString()
+               };
+             };
+
              scope.onClickSeekBar = function(event) {
                var percent = calculatePercent(seekBar, event);
                scope.value = percent * scope.max;
